3b87f77cc17202b30fe12c40c52e8bac1136bcc4
Quan Dai Tri <qdaitridn2003@gmail.com>
2023-06-08 14:36:41 +0700

    Test

---
+++ src/components/CatalogButton.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function CatalogButton(props) {
+  return (
+    <View>
+      <Text>CatalogButton</Text>
+    </View>
+  )
+}
